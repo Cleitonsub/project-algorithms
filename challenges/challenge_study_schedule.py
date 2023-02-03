6cfeaca0acf8ab594422ed275bfa53a23061befe
Cleiton <cleitonrmacedo@gmail.com>
2023-02-03 15:30:07 -0300

    feat: Reuisito 1 -  Numero de estudantes estudando no mesmo horario (Algoritmo de busca)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    if not isinstance(target_time, int):
+        return None
+    for tupla in permanence_period:
+        if isinstance(tupla[0], int) and isinstance(tupla[1], int):
+            if tupla[0] <= target_time and tupla[1] >= target_time:
+                count += 1
+        else:
+            return None
+
+    return count
