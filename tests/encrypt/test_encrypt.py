4e8effb481376be214898f5cccbbbd8ce123188b
Cleiton <cleitonrmacedo@gmail.com>
2023-02-03 16:20:34 -0300

    test: Requisito 2 - Criptografia de inversões

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    message = "World!"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(message, "x")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1,2)
+
+    assert encrypt_message(message, 2) == "!dlr_oW"
+    assert encrypt_message(message, 3) == "roW_!dl"
+    assert encrypt_message(message, -2) == "!dlroW"
