ae03208892179bfde7abdef97bdce0f4cd85fa76
Cleiton <cleitonrmacedo@gmail.com>
2023-02-10 13:53:27 -0300

    fix: Linha 2: E261 at least two spaces before inline comment

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,5 +1,6 @@
 def is_palindrome_iterative(word):
-    if word == "" or word != word[::-1]: # word[::-1] string reverse
+    if word == "" or word != word[::-1]:
+        # word[::-1] string reverse
         return False
 
     return True
